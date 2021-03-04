iNorthSea/gatsby-advanced-starter

Add new frontmatter variables for additional capabilities.

gatsby-config.js
@@ -147,7 +147,7 @@ module.exports = {
               const { rssMetadata } = ctx.query.site.siteMetadata;
               return ctx.query.allMarkdownRemark.edges.map((edge) => ({
                 categories: edge.node.frontmatter.tags,
-                date: edge.node.fields.date,
+                date: edge.node.fields.datePublished,
                 title: edge.node.frontmatter.title,
                 description: edge.node.excerpt,
                 url: rssMetadata.site_url + edge.node.fields.slug,
@@ -162,7 +162,7 @@ module.exports = {
             {
               allMarkdownRemark(
                 limit: 1000,
-                sort: { order: DESC, fields: [fields___date] },
+                sort: { order: DESC, fields: [fields___datePublishedPublished] },
               ) {
                 edges {
                   node {
@@ -171,12 +171,12 @@ module.exports = {
                     timeToRead
                     fields {
                       slug
-                      date
+                      datePublished
                     }
                     frontmatter {
                       title
                       cover
-                      date
+                      datePublished
                       category
                       tags
                     }

src/templates/category.jsx
@@ -27,7 +27,7 @@ export const pageQuery = graphql`
   query CategoryPage($category: String) {
     allMarkdownRemark(
       limit: 1000
-      sort: { fields: [fields___date], order: DESC }
+      sort: { fields: [fields___datePublished], order: DESC }
       filter: { frontmatter: { category: { eq: $category } } }
     ) {
       totalCount
@@ -35,7 +35,7 @@ export const pageQuery = graphql`
         node {
           fields {
             slug
-            date
+            datePublished
           }
           excerpt
           timeToRead
@@ -43,7 +43,7 @@ export const pageQuery = graphql`
             title
             tags
             cover
-            date
+            datePublished
           }
         }
       }

src/templates/listing.jsx
@@ -59,7 +59,7 @@ export default Listing;
 export const listingQuery = graphql`
   query ListingQuery($skip: Int, $limit: Int) {
     allMarkdownRemark(
-      sort: { fields: [fields___date], order: DESC }
+      sort: { fields: [fields___datePublished], order: DESC }
       limit: $limit
       skip: $skip
     ) {
@@ -67,7 +67,7 @@ export const listingQuery = graphql`
         node {
           fields {
             slug
-            date
+            datePublished
           }
           excerpt
           timeToRead
@@ -75,7 +75,7 @@ export const listingQuery = graphql`
             title
             tags
             cover
-            date
+            datePublished
           }
         }
       }

src/templates/tag.jsx
@@ -25,7 +25,7 @@ export const pageQuery = graphql`
   query TagPage($tag: String) {
     allMarkdownRemark(
       limit: 1000
-      sort: { fields: [fields___date], order: DESC }
+      sort: { fields: [fields___datePublished], order: DESC }
       filter: { frontmatter: { tags: { in: [$tag] } } }
     ) {
       totalCount
@@ -33,7 +33,7 @@ export const pageQuery = graphql`
         node {
           fields {
             slug
-            date
+            datePublished
           }
           excerpt
           timeToRead
@@ -41,7 +41,7 @@ export const pageQuery = graphql`
             title
             tags
             cover
-            date
+            datePublished
           }
         }
       }

ysabzd/gatsby-advanced-starter-1

gatsby-config.js
@@ -147,7 +147,7 @@ module.exports = {
               const { rssMetadata } = ctx.query.site.siteMetadata;
               return ctx.query.allMarkdownRemark.edges.map((edge) => ({
                 categories: edge.node.frontmatter.tags,
-                date: edge.node.fields.date,
+                date: edge.node.fields.datePublished,
                 title: edge.node.frontmatter.title,
                 description: edge.node.excerpt,
                 url: rssMetadata.site_url + edge.node.fields.slug,
@@ -162,7 +162,7 @@ module.exports = {
             {
               allMarkdownRemark(
                 limit: 1000,
-                sort: { order: DESC, fields: [frontmatter___date] },
+                sort: { order: DESC, fields: [frontmatter___datePublished] },
               ) {
                 edges {
                   node {
@@ -171,12 +171,11 @@ module.exports = {
                     timeToRead
                     fields {
                       slug
-                      date
                     }
                     frontmatter {
                       title
                       cover
-                      date
+                      datePublished
                       category
                       tags
                     }

src/templates/category.jsx
@@ -27,7 +27,7 @@ export const pageQuery = graphql`
   query CategoryPage($category: String) {
     allMarkdownRemark(
       limit: 1000
-      sort: { fields: [frontmatter___date], order: DESC }
+      sort: { fields: [frontmatter___datePublished], order: DESC }
       filter: { frontmatter: { category: { eq: $category } } }
     ) {
       totalCount
@@ -35,7 +35,6 @@ export const pageQuery = graphql`
         node {
           fields {
             slug
-            date
           }
           excerpt
           timeToRead
@@ -43,7 +42,7 @@ export const pageQuery = graphql`
             title
             tags
             cover
-            date
+            datePublished
           }
         }
       }

src/templates/listing.jsx
@@ -59,7 +59,7 @@ export default Listing;
 export const listingQuery = graphql`
   query ListingQuery($skip: Int, $limit: Int) {
     allMarkdownRemark(
-      sort: { fields: [frontmatter___date], order: DESC }
+      sort: { fields: [frontmatter___datePublished], order: DESC }
       limit: $limit
       skip: $skip
     ) {
@@ -67,7 +67,6 @@ export const listingQuery = graphql`
         node {
           fields {
             slug
-            date
           }
           excerpt
           timeToRead
@@ -75,7 +74,7 @@ export const listingQuery = graphql`
             title
             tags
             cover
-            date
+            datePublished
           }
         }
       }

src/templates/tag.jsx
@@ -25,7 +25,7 @@ export const pageQuery = graphql`
   query TagPage($tag: String) {
     allMarkdownRemark(
       limit: 1000
-      sort: { fields: [frontmatter___date], order: DESC }
+      sort: { fields: [frontmatter___datePublished], order: DESC }
       filter: { frontmatter: { tags: { in: [$tag] } } }
     ) {
       totalCount
@@ -33,7 +33,6 @@ export const pageQuery = graphql`
         node {
           fields {
             slug
-            date
           }
           excerpt
           timeToRead
@@ -41,7 +40,7 @@ export const pageQuery = graphql`
             title
             tags
             cover
-            date
+            datePublished
           }
         }
       }